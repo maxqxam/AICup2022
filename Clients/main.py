1ea24f1555e1f1ff468e77bfbcf127ac4a6a8e67
alikarimiDotnet <karimyai46@gmail.com>
2022-09-08 23:51:43 -0700

    Add a star algorithm for routing Add functions: get_connected_nodes and Connected_nodes2dictionary. for help a star

--- Clients/main.py
+++ Clients/main.py
@@ -1,9 +1,90 @@
+from calendar import c
+from distutils.log import debug
+from logging import exception
 from os import makedirs
 from enum import Enum
 import random
-import re
 DEBUG = 1
 
+def a_star_algorithm(self,map_gride,start_node, stop_node):
+
+    open_list = set([start_node])
+    closed_list = set([])
+    g = {}
+    g[start_node] = 0
+    parents = {}
+    parents[start_node] = start_node
+
+    while len(open_list) > 0:
+        n = None
+        for v in open_list:
+            if n == None or g[v] + h(v) < g[n] + h(n):
+                n = v
+
+        if n == None:
+            return None
+        if n == stop_node:
+            reconst_path = []
+
+            while parents[n] != n:
+                reconst_path.append(n)
+                n = parents[n]
+
+            reconst_path.append(start_node)
+
+            reconst_path.reverse()
+            return reconst_path
+        
+        for (m, weight) in Connected_nodes2dictionary(self,n):
+            if m not in open_list and m not in closed_list:
+                open_list.add(m)
+                parents[m] = n
+                g[m] = g[n] + weight
+
+            else:
+                if g[m] > g[n] + weight:
+                    g[m] = g[n] + weight
+                    parents[m] = n
+
+                    if m in closed_list:
+                        closed_list.remove(m)
+                        open_list.add(m)
+
+        open_list.remove(n)
+        closed_list.add(n)
+    return None
+def h( n):
+    return 1 
+def convert_strlist_to_int(str):# '[3,4]'--->(3,4)
+    a=str.split(',')
+    b=a[0]
+    c=b.split('[')
+    d=a[1]
+    e=d.split(']')
+    return int(c[1]),int(e[0])
+
+def get_connected_nodes(self,coordinates):
+    map_gride=self.map.grid
+    i,j=coordinates
+    list_coordinates=[[i,j+1],[i,j-1],[i-1,j],[i+1,j]]
+    for i in map_gride:
+        if i.coordinates in list_coordinates and i.type==4:
+            list_coordinates.remove(i.coordinates)
+    return list_coordinates
+
+
+def Connected_nodes2dictionary(self,coordinates):
+    c,w=convert_strlist_to_int(coordinates)
+    poslist=get_connected_nodes(self,(c,w))
+    res=[]
+    for  element in (poslist):
+        
+        res.append(( f'{element}',1 ))
+             
+    dic={
+        f'{coordinates}': res
+    }
+    return dic[coordinates]
 
 class Action(Enum):
     def __str__(self) -> str:
@@ -96,8 +177,9 @@ class GameState:
         self.debug_log += f'safe wallet: {str(self.safe_wallet)}\n'
         self.debug_log += f'list of wallets: {str(self.wallets)}\n'
         self.debug_log += f'last action: {str(self.last_action)}\n'
+        
+        # self.debug_log += f'self.map.grid {str(a=get_connected_nodes(self.map.grid,0.0))}\n'
         self.debug_log += f'{60 * "-"}\n'
-
     def debug_file(self) -> None:
         fileName = 'Clients/logs/'
         makedirs(fileName, exist_ok=True)
@@ -105,29 +187,33 @@ class GameState:
         with open(fileName, 'a') as f:
             f.write(self.debug_log)
 
+   
+
+
     def get_action(self) -> Action:
         # write your code here
         # return the action value
-        target = (3,3)
+        start=list(self.location)
+        path=a_star_algorithm(self,self.map.grid,str(start),str([5,5]))
+        if len(path)>1:
+            path.pop(0)
 
-        for i in self.map.grid:
-            if i.data==MapType.GOLD:
-                target = i.coordinates
+        goal=convert_strlist_to_int(path.pop(0))
+        
+        self.debug_log += f'goal {str(goal)}\n'
+        return getStepTowards(start,goal)
+def getStepTowards(source,destination) -> Action:
+    if source[0] < destination[0]:
+        return Action.MOVE_DOWN
+    if source[0] > destination[0]:
+        return Action.MOVE_UP
 
+    if source[1] < destination[1]:
+        return Action.MOVE_RIGHT
+    if source[1] > destination[1]:
+        return Action.MOVE_LEFT
 
-        x,y=target
-        x1,y1=self.location
-        if x>x1:
-            return Action.MOVE_DOWN
-        if x<x1:
-            return Action.MOVE_UP
-
-        if y>y1:
-            return Action.MOVE_RIGHT
-        if y<y1:
-            return Action.MOVE_LEFT
-
-
+    return Action.STAY
 if __name__ == '__main__':
     game_state = GameState()
     for _ in range(game_state.rounds):
